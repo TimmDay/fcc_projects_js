8b187cb48d716ee6eea9e2439b10115d95dcbc03
Tim Day <timmday.info@gmail.com>
2018-04-06 14:28:43 +0200

    update js to include function bodies for game state

--- tictactoe/scripts/tictactoe.js
+++ tictactoe/scripts/tictactoe.js
@@ -9,6 +9,7 @@ document.getElementById('app').textContent = 'test';
 //  6g   7h   8i
 
 let isTurnX = true; //x always goes first
+let isOpponentComputer = false;
 
 const checkWinConditions = () => {
     // check if 3 in a row are the same
@@ -25,28 +26,48 @@ const checkWinConditions = () => {
     const i = document.getElementById('pos8').textContent;
 
     if (a === b && b === c){
-        console.log('game over abc');
+        console.log('game over abc. Winner is: ' + document.getElementById(c));
     } else if (d === e && e === f) {
-        console.log('game over def');
+        console.log('game over def. Winner is: ' + document.getElementById(f));
     } else if (g === h && h === i) {
-        console.log('game over ghi');
+        console.log('game over ghi. Winner is: ' + document.getElementById(i));
     } else if (a === d && d === g) {
-        console.log('game over adg');
+        console.log('game over adg. Winner is: ' + document.getElementById(g));
     } else if (b === e && e === h) {
-        console.log('game over beh');
+        console.log('game over beh. Winner is: ' + document.getElementById(h));
     } else if (c === f && f === i) {
-        console.log('game over cfi');
+        console.log('game over cfi. Winner is: ' + document.getElementById(i));
     } else if (a === e && e === i) {
-        console.log('game over aei');
+        console.log('game over aei. Winner is: ' + document.getElementById(i));
     } else if (c === e && e === g) {
-        console.log('game over ceg');
+        console.log('game over ceg. Winner is: ' + document.getElementById(g));
     }
 };
 
 const toggleTurn = () => isTurnX = !isTurnX;
 
+const toggleOpponent = () => {
+    // toggle up top. play against: human/computer
+};
+
+const chooseTeam = () => {
+    // toggle button up top for player to choose who goes first
+    // depending on choice, toggle isTurnX
+    // after clicked, button is disabled
+    // after clicked, display message: player 1 is X/O. Computer/player 2 is X/O
+};
+
+const resetNewGame = () => {
+    // re-enable choose team button
+    // empty all spaces
+};
+
+const computerMakeMove = () => {
+
+};
 
 
+// PLAYER 1 Select Move
 
 document.getElementById('pos0').addEventListener('click', function(){
     // check if space is empty. if not do nothing
@@ -97,7 +118,6 @@ document.getElementById('pos2').addEventListener('click', function(){
 
 
 document.getElementById('pos3').addEventListener('click', function(){
-
     const spaceOccupant = document.getElementById('pos3').textContent;
     if (spaceOccupant !== 'position 3'){ //todo update content to empty string after styles
         console.log('space occupied');
@@ -108,8 +128,8 @@ document.getElementById('pos3').addEventListener('click', function(){
     checkWinConditions();
     toggleTurn();
 });
-document.getElementById('pos4').addEventListener('click', function(){
 
+document.getElementById('pos4').addEventListener('click', function(){
     const spaceOccupant = document.getElementById('pos4').textContent;
     if (spaceOccupant !== 'position 4'){ //todo update content to empty string after styles
         console.log('space occupied');
@@ -121,9 +141,7 @@ document.getElementById('pos4').addEventListener('click', function(){
     toggleTurn();
 });
 
-
 document.getElementById('pos5').addEventListener('click', function(){
-
     const spaceOccupant = document.getElementById('pos5').textContent;
     if (spaceOccupant !== 'position 5'){ //todo update content to empty string after styles
         console.log('space occupied');
@@ -137,7 +155,6 @@ document.getElementById('pos5').addEventListener('click', function(){
 
 
 document.getElementById('pos6').addEventListener('click', function(){
-
     const spaceOccupant = document.getElementById('pos6').textContent;
     if (spaceOccupant !== 'position 6'){ //todo update content to empty string after styles
         console.log('space occupied');
@@ -150,7 +167,6 @@ document.getElementById('pos6').addEventListener('click', function(){
 });
 
 document.getElementById('pos7').addEventListener('click', function(){
-
     const spaceOccupant = document.getElementById('pos7').textContent;
     if (spaceOccupant !== 'position 7'){ //todo update content to empty string after styles
         console.log('space occupied');
@@ -163,7 +179,6 @@ document.getElementById('pos7').addEventListener('click', function(){
 });
 
 document.getElementById('pos8').addEventListener('click', function(){
-
     const spaceOccupant = document.getElementById('pos8').textContent;
     if (spaceOccupant !== 'position 8'){ //todo update content to empty string after styles
         console.log('space occupied');
